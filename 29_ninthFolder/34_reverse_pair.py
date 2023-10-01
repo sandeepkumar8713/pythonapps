c35970690a784900ff293a5f1d08c65dd6dd3e0b
sandeepkumar8713 <sandeepkumar8713@gmail.com>
2023-10-01 22:12:10 +0530

    q added

---
+++ 29_ninthFolder/34_reverse_pair.py
@@ -0,0 +1,13 @@
+# You are given an array of integers of size n. Your task is to count the number of pairs (i, j) which f
+# ollows below conditions:
+# => 0 < i < j < n
+# => arr[i] + reverse(arr[j]) == arr[j] + reverse(arr[i])
+# Examples:
+# [42, 32, 53, 76, 13] -> 2
+# 1. (42, 53) => 42 + 35 == 24 + 53
+# 2. (32, 76) => 32 + 67 == 23 + 76
+#
+# Question Type : Asked
+# TODO :: add code
+
+
---
+++ 29_ninthFolder/35_apple_freshness.py
@@ -0,0 +1,28 @@
+# You are getting one apple everyday represented by an array of size n. each element of array denotes the
+# freshness of apple. freshness = 0 means the apple is already not consumable.
+# freshness = n means the apple can be consumed for n days
+# You are given a second array consisting of only 1s and 0s which denotes whether there was a beggar on your
+# doorstep on that particular day. If there was a beggar you give one apple to him. Your job is to figure out if
+# you can feed him one apple every day or not
+#
+# Input: Apples: [1, 2, 8, 3, 1]
+# Beggar Presence: [1, 8, 1, 1, 0]
+# Expected : True
+#
+# Question Type : Asked
+# TODO :: add code
+
+# SQL
+# find 10 toppers (aggregate percentage) considering best 4 subjects
+#
+# Rollnumber, Term, Subject, Marks
+# R1 T1 S1 48
+# R1 T2 S1 50
+# R1 T2 S2 50
+# R2 T1 S1 10
+# R2 T2 S1 20
+# R20 T1 S1 20
+# R20 T2 S2 80
+#
+# There can be any number of sems and any num of subjects in each sem
+#
